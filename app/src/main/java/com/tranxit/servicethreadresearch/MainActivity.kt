4c59d153beceb7900ca9660376ee12e84039fcf6
Suresh <suresh.s@appoets.com>
2019-11-16 13:06:35 +0530

    inital commits

--- app/src/main/java/com/tranxit/servicethreadresearch/MainActivity.kt
+++ app/src/main/java/com/tranxit/servicethreadresearch/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.tranxit.servicethreadresearch
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tranxit.servicethreadresearch.service.MyownService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startService(Intent(this, MyownService::class.java))
     }
 }
